c82c2d39a6e7eb24fd66cb23194909c9109d762a
Scott Meade <scott@synapsoftware.com>
2015-09-18 17:48:45 -0600

    Iteration B1: Validating!

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url: 'lorem.jpg',
+      price: 19.95
+    }
   end
 
   test "should get index" do
@@ -17,7 +23,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(Product.last)
@@ -34,7 +40,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, params: { id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch :update, id: @product, product: @update
     assert_redirected_to product_path(@product)
   end
 
