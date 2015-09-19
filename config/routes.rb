d6f769bf5b09bea9e75f8c40d6c8b4b74abcb56f
Scott Meade <scott@synapsoftware.com>
2015-09-19 08:57:09 -0600

    Iteration G2: Atom Feeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :line_items
+
   resources :carts
+  resources :line_items
+  resources :products do
+    get :who_bought, on: :member
+  end
+  resources :orders
+
   get 'store/index'
-
-  resources :products
-
   root 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
